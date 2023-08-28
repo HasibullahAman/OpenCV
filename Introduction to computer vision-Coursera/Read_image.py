6c9e67890080237839f4abbb9d5dda0b30fbbcbe
Hasibullah Aman <hasib.aman7@gmail.com>
2023-08-28 06:36:46 +0430

    store the changed image and read it again

--- Introduction to computer vision-Coursera/Read_image.py
+++ Introduction to computer vision-Coursera/Read_image.py
@@ -7,7 +7,14 @@ images = Image.open('../images/cat.png')
 # convert the image to grayscale
 image_to_gray =ImageOps.grayscale(images)
 # image_to_gray.show()
-print(image_to_gray.mode)
-print(image_to_gray.format)
-print(image_to_gray.width)
-print(image_to_gray.height)
+# print(image_to_gray.mode)
+# print(image_to_gray.format)
+# print(image_to_gray.width)
+# print(image_to_gray.height)
+
+# if after some change we want to save our image we use this commend
+image_to_gray.save('new_cat.png')
+
+# Read the save image
+new_image_cat = Image.open('./new_cat.png')
+new_image_cat.show()
