9537c1aadf18f7034f9701ee28a1820caaf328da
Hasibullah Aman <hasib.aman7@gmail.com>
2023-08-28 06:30:49 +0430

    connver image to grayscale

--- Introduction to computer vision-Coursera/Read_image.py
+++ Introduction to computer vision-Coursera/Read_image.py
@@ -2,5 +2,8 @@ import numpy as np
 from PIL import ImageOps
 from PIL import Image
 images = Image.open('../images/cat.png')
-images.show(images)
+# images.show(images)
 
+# convert the image to grayscale
+image_to_gray =ImageOps.grayscale(images)
+image_to_gray.show()
