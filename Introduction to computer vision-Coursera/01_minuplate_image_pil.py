35a824eaf9c1114ca98184c74bd0b7f91d410aab
Hasibullah Aman <hasib.aman7@gmail.com>
2023-10-02 15:28:49 +0430

    init some new file

--- Introduction to computer vision-Coursera/01_minuplate_image_pil.py
+++ Introduction to computer vision-Coursera/01_minuplate_image_pil.py
@@ -1,15 +1,5 @@
-import numpy as np
+import matplotlib.pyplot as plt
 from PIL import Image
-from PIL import ImageOps
 
-# in bellow, we work to show how we can get a copy from an image and how it changes the id in memory
-image = Image.open('../images/baboon.png')
+image = Image.open("../images/baboon.png")
 image.show()
-image_copy = image.copy()
-print(id(image))
-print(id(image_copy))
-
-# flip and mirror the image
-# how to flip the image
-image_flip =ImageOps.flip(image)
-image_flip.show()
