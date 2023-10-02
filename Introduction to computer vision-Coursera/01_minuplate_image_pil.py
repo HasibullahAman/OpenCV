81547530cd0119e8c418041580b4e3818c9bd795
Hasibullah Aman <hasib.aman7@gmail.com>
2023-10-02 15:47:19 +0430

    read image

--- Introduction to computer vision-Coursera/01_minuplate_image_pil.py
+++ Introduction to computer vision-Coursera/01_minuplate_image_pil.py
@@ -1,5 +1,17 @@
 import matplotlib.pyplot as plt
 from PIL import Image
+from PIL import ImageOps
 
-image = Image.open("../images/baboon.png")
+image = Image.open("../images/lenna.png")
+image.quantize(20)
 image.show()
+
+# image_gray = ImageOps.grayscale(image)
+# image_gray.show()
+# plt.imshow(image)
+print(image.format)
+print(image.size)
+print(image.mode)
+
+# image_gray.save('baboon2.jpg')
+
