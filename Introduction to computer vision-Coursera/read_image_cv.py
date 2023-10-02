47ae72fb2b02ee9292bb1b8397997ddf27a271e5
Hasibullah Aman <hasib.aman7@gmail.com>
2023-10-02 16:00:46 +0430

    read image

--- Introduction to computer vision-Coursera/read_image_cv.py
+++ Introduction to computer vision-Coursera/read_image_cv.py
@@ -1,11 +1,8 @@
+import numpy
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-image_path = "./new_cat.png"
-image = cv.imread(image_path)
-image_rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
-
-cv.imshow("cat",image_rgb)
-# plt.show()
-cv.waitKey(0)
+# image_path = "../images/lenna.png"
+image = cv.imread("../images/lenna.png")
+plt.imshow(image)
