6858591a4af366678f4b0f7f03c8dd3504a88e4d
Hasibullah Aman <hasib.aman7@gmail.com>
2024-07-04 20:12:03 +0430

    done resizing

--- 02_rescale.py
+++ 02_rescale.py
@@ -12,3 +12,33 @@ def rescaleFrame(frame,scale = 0.75):
     return cv.resize(frame,dimenations, interpolation = cv.INTER_AREA)
 
 
+
+# read image
+# frame = cv.imread("images/cat.png")
+# resize_frame = rescaleFrame(frame) # we resize the image here
+# cv.imshow("cat",resize_frame)
+# cv.imshow("same_cat",frame)
+# cv.waitKey(0 )
+
+
+
+# read Video  
+
+video = cv.VideoCapture("video/B.mp4")
+
+
+while True:
+    isTrue, frame = video.read()
+    resize_frame = rescaleFrame(frame)
+    
+    cv.imshow("Video",resize_frame)
+    cv.imshow("Video_original",frame)
+    
+    
+    
+    if cv.waitKey(20) &  0xFF==ord('q'):
+        break
+    
+video.release()
+cv.destroyAllWindows()
+    
