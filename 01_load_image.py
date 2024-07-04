a42b24b6a6476c7cc36b21a68d500b00cae754da
Hasibullah Aman <hasib.aman7@gmail.com>
2024-07-04 19:14:35 +0430

    read video

--- 01_load_image.py
+++ 01_load_image.py
@@ -1,5 +1,21 @@
 import cv2 as cv
 
-img = cv.imread('.\images/baboon.png')
-cv.imshow('Cat', img)
-cv.waitKey(0)
+# img = cv.imread('.\images/baboon.png')
+
+# cv.imshow('Cat', img)
+# cv.waitKey(0)
+
+
+
+capture = cv.VideoCapture('video/B.mp4')
+while True:
+    isTrue, frame = capture.read()
+    
+    cv.imshow('Video', frame)
+        
+    if cv.waitKey(20) &  0xFF==ord('q'):
+        break
+capture.release()
+cv.destroyAllWindows()
+
+    
