2bf8cc1e857994264ffa9a81f583677efc09d5eb
Hasibullah Aman <hasib.aman7@gmail.com>
2024-07-10 18:31:24 +0430

    learn some function in openCv

--- 04_Esential_Functions.py
+++ 04_Esential_Functions.py
@@ -1,1 +1,13 @@
 import cv2 as cv
+
+img = cv.imread("images/cat.png")
+def rescaleFunction(frame,scale):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+    return cv.resize(frame,dimensions, interpolation = cv.INTER_AREA)
+
+changeFrame = rescaleFunction(img,0.75)
+cv.imshow("changeFrame",changeFrame)
+
+cv.waitKey(20000)
